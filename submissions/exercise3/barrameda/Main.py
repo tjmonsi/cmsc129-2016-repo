f29410fe073dfcfbc1814eb7ffd092e417b169bd
Simon <barramedasimon321@gmail.com>
2016-04-17 15:24:25 +0800

    Added sample codes without syntax errors

--- submissions/exercise3/barrameda/Main.py
+++ submissions/exercise3/barrameda/Main.py
@@ -1,18 +1,38 @@
 import LexicalAnalyzer
 import SyntaxAnalyzer
 
-#code = open('sample1.scb', 'r')
-#tokens = LexicalAnalyzer.tokenizer(code.read())
-
-#SyntaxAnalyzer.parser(tokens)
+code = open('sample1.scb', 'r')
+tokens = LexicalAnalyzer.tokenizer(code.read())
+print("Syntax Error for Sample 1")
+SyntaxAnalyzer.parser(tokens)
+print('\n')
 
 #code = open('sample2.scb', 'r')
 #tokens = LexicalAnalyzer.tokenizer(code.read())
-
+#print("Syntax Error for Sample 2")
 #SyntaxAnalyzer.parser(tokens)
+#print('\n')
 
-code = open('sample3.scb', 'r')
-tokens = LexicalAnalyzer.tokenizer(code.read())
+#code = open('sample3.scb', 'r')
+#tokens = LexicalAnalyzer.tokenizer(code.read())
+#print("Syntax Error for Sample 3")
+#SyntaxAnalyzer.parser(tokens)
+#print('\n')
 
-SyntaxAnalyzer.parser(tokens)
-print('\n')
+#code = open('sample4.scb', 'r')
+#tokens = LexicalAnalyzer.tokenizer(code.read())
+#print("Syntax Error for Sample 4")
+#SyntaxAnalyzer.parser(tokens)
+#print('\n')
+
+#code = open('sample5.scb', 'r')
+#tokens = LexicalAnalyzer.tokenizer(code.read())
+#print("Syntax Error for Sample 5")
+#SyntaxAnalyzer.parser(tokens)
+#print('\n')
+
+#code = open('sample6.scb', 'r')
+#tokens = LexicalAnalyzer.tokenizer(code.read())
+#print("Syntax Error for Sample 6")
+#SyntaxAnalyzer.parser(tokens)
+#print('\n')
